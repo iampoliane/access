5e0c99ed353b10b73f1f5f77c05de1d795dbabe5
Gabrielle Duarte <gabrielle.duarte@g.globo>
2023-11-21 19:14:54 -0300

    Adds Header, listpage and browser router

---
+++ src/pages/Dashboard/mock.js
@@ -0,0 +1,24 @@
+export const schedules = [{
+    id: '87989',
+    name: 'Ana Maria Braga',
+    time: {
+        start:'09:00',
+        end: '12:00',
+    }
+},
+{
+    id: '54357',
+    name: 'Ana Maria Braga',
+    time: {
+        start:'09:00',
+        end: '12:00',
+    }
+},
+{
+    id: '74587',
+    name: 'Ana Maria Braga',
+    time: {
+        start:'09:00',
+        end: '12:00',
+    }
+}]
