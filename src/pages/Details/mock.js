4dfb5921b817edee60da07646a5cd1a8fe89b1e4
Poliane Brito <polyanebritto@hotmail.com>
2023-11-23 21:27:56 -0300

    adds details page content

---
+++ src/pages/Details/mock.js
@@ -0,0 +1,13 @@
+export const TvShow = {
+    id: '87989',
+    name: 'Ana Maria Braga',
+    date: '27/11/2023',
+    type: 'Nacional',
+    recurrence: 'Dias úteis',
+    description: 'Comandado por Ana Maria Braga, o programa de culinária conta com receitas,' +
+    ' notícias, competições e as novidades da Globo.',
+    time: {
+        start:'09:00',
+        end: '12:00',
+    }
+}
