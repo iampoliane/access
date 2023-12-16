7c7fbccf52ec391a6efcbea550f9ced8f11c0f5a
Gabrielle Duarte <gabrielle.duarte@g.globo>
2023-12-16 15:52:31 -0300

    Fix list page

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,58 +1,60 @@
 // import { schedules } from "./mock.js";
-import React, { useState } from "react";
-import "./styles.css";
-import { Link } from "react-router-dom";
+import React, {useState} from 'react';
+import './styles.css';
+import {Link} from 'react-router-dom';
 import api from '../../services/api';
 
 export function Dashboard() {
-  const [searchTerm, setSearchTerm] = useState("");
+  const [searchTerm, setSearchTerm] = useState('');
   const [schedule, setSchedule] = useState([]);
 
-
-  const filteredSchedule = schedule.filter((schedule) =>
-    schedule.name.toLowerCase().includes(searchTerm.toLowerCase())
+  const filteredSchedule = schedule.filter(schedule =>
+    schedule.name.toLowerCase().includes(searchTerm.toLowerCase()),
   );
 
   const handleSearch = () => {
-    console.log("Pesquisar por:", searchTerm);
+    console.log('Pesquisar por:', searchTerm);
   };
-
 
   const getSchedule = React.useCallback(async () => {
     try {
-      const response = await api.get(`/list`)
+      const response = await api.get('/list');
 
-      setSchedule(response.data)
-    } catch (error) {
-
-    }
-  },)
+      setSchedule(response.data);
+    } catch (error) {}
+  }, []);
 
   React.useEffect(() => {
-    getSchedule()
+    getSchedule();
   }, [getSchedule]);
 
-  if (!schedule) return null;
-
+  if (!schedule) {
+    return null;
+  }
 
   // const params = useParams();//might not be needy
 
   return (
     <div className="container">
-      <a href="/Form" class="button-link">Adicionar Programa de TV</a>
+      <a href="/Form" className="button-link">
+        Adicionar Programa de TV
+      </a>
       <div className="search-bar">
         <input
           type="text"
           placeholder="Pesquisar..."
           value={searchTerm}
-          onChange={(e) => setSearchTerm(e.target.value)}
+          onChange={e => setSearchTerm(e.target.value)}
         />
-        <button onClick={handleSearch} class="button">Pesquisar</button>
+        <button onClick={handleSearch} className="button">
+          Pesquisar
+        </button>
       </div>
-      {filteredSchedule.map((schedule) => (
+      {filteredSchedule.map(schedule => (
         <Link className="card" key={schedule.id} to={schedule.id}>
           <span>{schedule.name}</span>
-          <span>{schedule.dateTime} - {schedule.dateTime}
+          <span>
+            {schedule.dateTime} - {schedule.dateTime}
           </span>
         </Link>
       ))}
